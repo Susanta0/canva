c979ebf1d7cf5567d09abacdfb4ee8cdd17cb5f4
Susanta0 <susanta721467@gmail.com>
2025-04-01 13:50:59 +0530

    checking

--- frontend/src/utils/api.js
+++ frontend/src/utils/api.js
@@ -1,12 +1,32 @@
+// import axios from "axios";
+
+// const localApi = `http://localhost:8000`;
+// const productionApi = `https://canva-mppp.onrender.com`;
+
+// const token = localStorage.getItem("canva_token");
+
+// const api = axios.create({
+//   baseURL: localApi,
+//   headers: {
+//     Authorization: token ? `Bearer ${token}` : "",
+//   },
+//   withCredentials: true,
+// });
+
+// export default api;
+
 import axios from "axios";
 
-const localApi = `http://localhost:8000`;
-const productionApi = `https://canva-mppp.onrender.com`;
+// Determine environment based on current URL
+const isProduction = window.location.hostname !== "localhost";
+const baseURL = isProduction
+  ? "https://canva-mppp.onrender.com"
+  : "http://localhost:8000";
 
 const token = localStorage.getItem("canva_token");
 
 const api = axios.create({
-  baseURL: productionApi,
+  baseURL,
   headers: {
     Authorization: token ? `Bearer ${token}` : "",
   },
